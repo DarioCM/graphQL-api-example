d8bfd563711ed8b3c14dea00f71d295f5f8cb4da
Carlos D <dario20049@icloud.com>
2024-06-28 00:43:21 +0900

    init

---
+++ src/main/java/com/example/graphql/orders/GraphQlApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphql/orders/GraphQlApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
